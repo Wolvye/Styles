52b479f53e9c6243891528ac21bd300bc715b215
Wolvye <alainsoltau@googlemail.com>
2025-06-27 12:02:18 +0200

    update

---
+++ Styles/DynamicStylesPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Styles;
+
+public partial class DynamicStylesPage : ContentPage
+{
+	public DynamicStylesPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Application.Current.Resources.TryGetValue("specialButton", out var retVal);
+		Resources["dynamicStyle"] = (Style)retVal; ;
+    }
+}
